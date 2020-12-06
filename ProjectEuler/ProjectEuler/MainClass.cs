7e02a7f057fdd1c1735b246280ed1549f3e943ca
Woods Hong <widewooz@gmail.com>
2020-12-06 18:10:08 +0900

    Solve Problem Five

--- ProjectEuler/ProjectEuler/MainClass.cs
+++ ProjectEuler/ProjectEuler/MainClass.cs
@@ -5,14 +5,12 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            //ProblemThree problemThree = new ProblemThree();
-            ProblemFour problemFour = new ProblemFour();
+            ProblemFive problemFive = new ProblemFive();
+
             Console.Clear();
 
-            //ProblemOne.Solve();
-            //ProblemTwo.Solve();
-            //problemThree.Solve(600851475143, 2);
-            problemFour.Solve();
+            problemFive.Solve();
         }
     }
 }
+
---
+++ ProjectEuler/ProjectEuler/Problems/ProblemFive.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+//Problem:L What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+namespace ProjectEuler
+{
+    public class ProblemFive
+    {
+        public void Solve()
+        {
+            int commonMultiple = 1;
+
+            for (int i = 2; i <= 20; i++)
+            {
+                commonMultiple = FindLeastCommonMultiple(commonMultiple, i);
+            }
+
+            Console.WriteLine(commonMultiple);
+        }
+
+        int FindLeastCommonMultiple(int num1, int num2)
+        {
+            int leastCommonMultiple = 0;
+
+            for(int i = 1; i <= num2; i++)
+            {
+                for (int j = 1; j <= num1; j++)
+                {
+                    if(num1 * i == num2 * j)
+                    {
+                        leastCommonMultiple = num1 * i;
+                        return leastCommonMultiple;
+                    }
+                }
+            }
+            return leastCommonMultiple;
+        }
+    }
+}
