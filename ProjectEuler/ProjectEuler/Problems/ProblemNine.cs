ff80a9631999e1bc5e7d58a8d89c56360bb0cd84
Woods Hong <widewooz@gmail.com>
2020-12-08 14:23:40 +0900

    Solve Problem Nine

---
+++ ProjectEuler/ProjectEuler/Problems/ProblemNine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    public class ProblemNine
+    {
+        List<int[]> pythagoreanTriplets = new List<int[]>();
+
+        public void Solve(int sumOfTriplet)
+        {
+            int a = 1, b = 2, c = 3;
+
+            for(c = 3; a+b+c <= sumOfTriplet; c++)
+            {
+                for(b = 2; b < c; b++)
+                {
+                    for(a = 1; a < b; a++)
+                    {
+                        if(MathF.Pow(a, 2) + MathF.Pow(b, 2) == MathF.Pow(c, 2))
+                        {
+                            pythagoreanTriplets.Add(new int[] { a, b, c });
+                            if(a + b + c == sumOfTriplet)
+                            {
+                                Console.WriteLine("The pythagorean triplet with sum " + sumOfTriplet + " is " + a + " " + b + " " + c);
+                            }
+                        }
+                    }
+                }
+                b = 2;
+                a = 1;
+            }
+        }
+    }
+}
