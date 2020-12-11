cd898f1af10baef2df876b47bc524334dca0531b
Woods Hong <widewooz@gmail.com>
2020-12-11 12:23:28 +0900

    Add a stopwatch to log elapsed time

--- ProjectEuler/ProjectEuler/MainClass.cs
+++ ProjectEuler/ProjectEuler/MainClass.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
+
 namespace ProjectEuler
 {
     class MainClass
-    {
+    { 
         static void Main(string[] args)
         {
+            Stopwatch sw = new Stopwatch();
+
             Problem14 problem = new Problem14();
 
             Console.Clear();
 
+            sw.Restart();
+
             problem.FindStartNumWithLargestSteps(1000000);
+
+            sw.Stop();
+
+            Console.WriteLine("\nTime Elapsed: " + sw.ElapsedMilliseconds + "ms");
         }
     }
 }
